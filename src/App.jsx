bf569206f206c20b0c127b1ddc1f7a90014f3fef
samuelmaina-rgb <maina.smbae@gmail.com>
2026-08-16 21:48:52 +0300

    add routing changes to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,29 @@
-import Navbar from "./Navbar"
-import ProductList from "./ProductList"
-import ProductCard from "./ProductCard"
-import ProductDetails from "./ProductDetails"
-import Cart from "./Cart"
-import SearchFilter from "./SearchFilter"
-import ProtectedRoute from "./ProtectedRoute"
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import ProductDetails from "./pages/ProductDetails";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+import Login from "./pages/Login";
 
 function App() {
     return (
         <>
             <Navbar />
-            <SearchFilter />
-            <ProductList />
-            <ProductCard />
-            <ProductDetails />
-            <Cart />
-            <ProtectedRoute />
+
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/products" element={<Products />} />
+                <Route path="/products/:id" element={<ProductDetails />} />
+                <Route path="/cart" element={<Cart />} />
+                <Route path="/checkout" element={<Checkout />} />
+                <Route path="/login" element={<Login />} />
+            </Routes>
         </>
-    )
+    );
 }
 
-export default App
+export default App;
