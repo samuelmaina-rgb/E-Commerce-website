059eb854752046aaa1bf268e644831231179629c
samuelmaina-rgb <maina.smbae@gmail.com>
2026-08-16 22:13:55 +0300

    add the function 'ProductCard' that helps us filter the data from the API

--- src/ProductCard.jsx
+++ src/ProductCard.jsx
@@ -1,9 +1,22 @@
-function ProductCard() {
+function ProductCard({ product }) {
     return (
-        <div>
-            <h3>Product</h3>
+        <div className="rounded-lg border bg-white p-4 shadow-sm">
+            
+            <img
+                src={product.image}
+                alt={product.title}
+                className="h-48 w-full object-contain"
+            />
+
+            <h2 className="mt-4 font-semibold text-gray-900">
+                {product.title}
+            </h2>
+
+            <p className="mt-2 text-lg font-bold text-indigo-600">
+                ${product.price}
+            </p>
         </div>
-    )
+    );
 }
 
-export default ProductCard
+export default ProductCard;
