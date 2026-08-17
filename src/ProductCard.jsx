77661cc74e3231a96a0e542adb8a7342ff5eda04
samuelmaina-rgb <maina.smbae@gmail.com>
2026-08-17 09:05:18 +0300

    add styling to ProductCard.jsx

--- src/ProductCard.jsx
+++ src/ProductCard.jsx
@@ -1,18 +1,18 @@
+
 function ProductCard({ product }) {
     return (
-        <div className="rounded-lg border bg-white p-4 shadow-sm">
-            
+        <div className="border p-4">
             <img
                 src={product.image}
                 alt={product.title}
-                className="h-48 w-full object-contain"
+                className="h-40 w-full object-contain"
             />
 
-            <h2 className="mt-4 font-semibold text-gray-900">
+            <h2 className="mt-3 font-semibold">
                 {product.title}
             </h2>
 
-            <p className="mt-2 text-lg font-bold text-indigo-600">
+            <p className="mt-2 font-bold">
                 ${product.price}
             </p>
         </div>
